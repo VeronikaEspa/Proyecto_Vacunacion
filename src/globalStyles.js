ed2356359a00bfbbf2b203eedbb6288b41f863c3
veronika <vesparragozam@gmail.com>
2021-07-29 11:37:22 -0500

    Añadiendo las fuentes en globalstyles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,5 +1,9 @@
 import {createGlobalStyle} from 'styled-components'
-// import PrimaryFont from './utils/fonts/Spartan-Medium.ttf'
+import PoppinsBold from './utils/fonts/Poppins-Bold.woff'
+import PoppinsLight from './utils/fonts/Poppins-Light.woff'
+import PoppinsMedium from './utils/fonts/Poppins-Medium.woff'
+import PoppinsRegular from './utils/fonts/Poppins-Regular.woff'
+import PoppinsSemiBold from './utils/fonts/Poppins-SemiBold.woff'
 const GlobalStyles = createGlobalStyle `
 html{ //Para que el scroll no sea de golpe
     scroll-behavior: smooth;
@@ -48,5 +52,30 @@ button{
     cursor: pointer;
 }
 // FUENTES
+@font-face {
+    font-family: "poppins-light";
+    src: local("poppins-light"), url(${PoppinsLight}) format("woff");
+    font-weight: normal;
+}
+@font-face {
+    font-family: "poppins-regular";
+    src: local("poppins-regular"), url(${PoppinsRegular}) format("woff");
+    font-weight: normal;
+}
+@font-face {
+    font-family: "poppins-medium";
+    src: local("poppins-medium"), url(${PoppinsMedium}) format("woff");
+    font-weight: normal;
+}
+@font-face {
+    font-family: "poppins-semibold";
+    src: local("poppins-semibold"), url(${PoppinsSemiBold}) format("woff");
+    font-weight: normal;
+}
+@font-face {
+    font-family: "poppins-bold";
+    src: local("poppins-bold"), url(${PoppinsBold}) format("woff");
+    font-weight: normal;
+}
 `
 export default GlobalStyles;
