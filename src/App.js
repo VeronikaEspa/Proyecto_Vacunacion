ca53df29aa7978806bbf35854df8cf97b1fc88cc
Katerin_Galindo <katealejandra22@gmail.com>
2021-07-30 11:53:41 -0500

    Elimine errores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import GlobalStyles from './globalStyles';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home/Home';
 import Login from './pages/Contacto/Login';
-import { Links } from './component';
 function App() {
   return (
     <div className="App">
