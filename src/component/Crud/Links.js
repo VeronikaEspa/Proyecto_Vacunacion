615297ae4aeef890e260c663c3e55532b6818e63
veronika <vesparragozam@gmail.com>
2021-08-05 10:12:36 -0500

    Arreglando las rutas del crud

--- src/component/Crud/Links.js
+++ src/component/Crud/Links.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import LinkForm from './LinkForm'
 import {db} from '../../firebase'
+import firebase from 'firebase'
 import './Tablas.estilos.css';
 import "./HeaderCrud.estilos.css";
 import {toast} from 'react-toastify'
 import lapiz from '../../utils/img/lapiz-editar.svg'
-
-const Links = ({handleLogout}) => {
+import {handleLogout} from '../Login/configLogin'
+const Links = () => {
 
     const [links, setLinks] = useState([]);
     const[currentId, setCurrentId] = useState("");
@@ -40,7 +41,7 @@ const Links = ({handleLogout}) => {
             const docs = [];
             querySnapshot.forEach((doc) => {
                 docs.push({...doc.data(), id:doc.id});
-                // console.log(docs)
+                console.log(docs)
         });
         setLinks(docs);
     });
@@ -49,6 +50,15 @@ const Links = ({handleLogout}) => {
     useEffect( () => {
         getLinks();
     }, []);
+    // state = {
+    //     divcontainer : false,
+    // }
+    // const HandleChange = e => {
+    //     this.setState({divcontainer:!this.state.divcontainer});
+    //     };
+    // const x = this.state.divcontainer;
+
+
 
     function cerrarSesion(boton){
         boton = document.getElementById("botonCerrarSesion");
@@ -58,12 +68,16 @@ const Links = ({handleLogout}) => {
         formu = document.getElementById("formulario");
         formu.classList.toggle("FormularioInscrip");
     };
+    const salir = () => {
+        firebase.auth().signOut()
+        window.location.replace("/Login")
+    };
     return(
     <div>
         <header className="menuCrud naranja">
         <button onClick={cerrarSesion} className="UsuarioNombre blancoFondo">
-            <h3>Bienvenidos, Juan Pablo</h3>
-        <button className="botonCerrarSesion naranja blancoLetra" id="botonCerrarSesion" onClick={handleLogout}>Cerrar sesión</button>
+            <h3>Bienvenido, Juan Pablo Corredor</h3>
+        <button className="botonCerrarSesion naranja blancoLetra" id="botonCerrarSesion" onClick={salir}>Cerrar sesión</button>
         </button>
         </header>
         <div className="encimaTabla">
@@ -104,22 +118,22 @@ const Links = ({handleLogout}) => {
                     </tr>
                     </thead>
                     <tbody id="tabla">
-        {links.map(docu => (
+        {links.map(link => (
                     <tr>
-                        <td className="tamañoPequeño">{docu.tipoDocumentos}</td>
-                        <td className="tamañoMediano">{docu.documento}</td>
-                        <td className="tamañoGrande">{docu.nombre}</td>
-                        <td className="tamañoGrande">{docu.correo}</td>
-                        <td className="tamañoMediano">{docu.telefono}</td>
-                        <td className="tamañoMediano">{docu.direccion}</td>
-                        <td className="tamañoGrande">{docu.sintomas}</td>
-                        <td className="tamañoPequeño">{docu.dosis}</td>
-                        <td className="tamañoMediano">{docu.laboratorio}</td>
+                        <td className="tamañoPequeño">{link.tipoDocumentos}</td>
+                        <td className="tamañoMediano">{link.documento}</td>
+                        <td className="tamañoGrande">{link.nombre}</td>
+                        <td className="tamañoGrande">{link.correo}</td>
+                        <td className="tamañoMediano">{link.telefono}</td>
+                        <td className="tamañoMediano">{link.direccion}</td>
+                        <td className="tamañoGrande">{link.sintomas}</td>
+                        <td className="tamañoPequeño">{link.dosis}</td>
+                        <td className="tamañoMediano">{link.laboratorio}</td>
                         <td className="botonesOpciones tamañoMediano">
-                            <button className="botonEditar" onClick={() => setCurrentId(docu.id)}>
+                            <button className="botonEditar" onClick={() => setCurrentId(link.id)}>
                             <img src={lapiz} alt="Editar"/>
                             </button>
-                            <button className="botonEliminar" onClick={() => onDeleteLink(docu.id)}>X</button>
+                            <button className="botonEliminar" onClick={() => onDeleteLink(link.id)}>X</button>
                         </td>
                     </tr>
             ))}
